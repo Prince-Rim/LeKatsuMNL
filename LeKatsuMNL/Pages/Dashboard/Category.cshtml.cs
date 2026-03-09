ea3877377c08b0b3b547266384c35890568bf9ec
Justin Allen Azucena <azucenajustinallen@gmail.com>
2026-03-09 13:40:17 +0800

    Added category and reports. Added new item and reject modal on transaction module.

---
+++ LeKatsuMNL/Pages/Dashboard/Category.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LeKatsuMNL.Pages.Dashboard
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
