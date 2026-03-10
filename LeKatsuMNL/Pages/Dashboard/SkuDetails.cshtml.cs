552ad6a6428116f8238a394f88b986300e24da28
Justin Allen Azucena <azucenajustinallen@gmail.com>
2026-03-10 23:46:34 +0800

    feat: Improved UI, added, transaction module, and SKU.

---
+++ LeKatsuMNL/Pages/Dashboard/SkuDetails.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LeKatsuMNL.Pages.Dashboard
+{
+    public class SkuDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
